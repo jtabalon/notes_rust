2e287afbbf506fd56ec15d3506e517ee566df7c6
jtabalon <joseph@tabalon.io>
2022-08-06 20:30:30 -0700

    add args len section

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,11 @@ use std::io::prelude::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    // Ok(())
+
+    if args.len() != 2 {
+        Err("Usage: notes your_note_goes_here")?;
+        std::process::exit(code: 1);
+    }
+
+    Ok(())
 }
