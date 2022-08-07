8a884ae97d6ab8271de6fc6c671b0a10e972ee90
jtabalon <joseph@tabalon.io>
2022-08-06 20:25:41 -0700

    add result output, and add args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    // Ok(())
 }
